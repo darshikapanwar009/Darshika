e0cb0c845a9273b5c23b1aa6aa7bd6d3963417b0
Darshika <136146993+darshikapanwar009@users.noreply.github.com>
2023-09-05 07:05:45 -0700

    progm1.java

---
+++ progm1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class print
+  {
+    public static void main(String args[])
+    {
+      Scanner sc = new Scanner(System.in);
+      int a= sc.nextInt();
+      System.out.println("print the integer entered by user ="+a);
+    }
+  }
