c0e3f90651e54076c5df47d6aefb98732cfc103c
Darshika <136146993+darshikapanwar009@users.noreply.github.com>
2023-09-05 08:23:15 -0700

    progm5.java

--- progm5.java
+++ progm5.java
@@ -4,6 +4,7 @@ class alpha
     public static void main(String[]args)
     {
     Scanner sc= new Scanner(System.in);
+      System.out.println("enter any alphabet:");
        char ch=sc.next().charAt(0);
       if(ch=='a'|| ch=='i'|| ch=='e'|| ch=='o' || ch=='u')
       {
