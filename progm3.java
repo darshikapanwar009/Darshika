b66fde2acbe27776087c43f945543f6f896e4cd0
Darshika <136146993+darshikapanwar009@users.noreply.github.com>
2023-09-05 07:34:34 -0700

    progm3.java

---
+++ progm3.java
@@ -0,0 +1,17 @@
+class swap
+  {
+    public static void main(String[]args)
+    {
+      int e=55;
+      int f=99;
+      //printing the values of e and f before swapping
+      System.out.println("before swapping : e=" +e+ "f=" +f);
+      int tempo=e;
+      e=f;
+      f=tempo;
+      //printing the values of e and f after swapping
+      System.out.println("after swapping : e=" +e+ "f=" +f);
+    }
+  }
+
+      
