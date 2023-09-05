349b98b8bcd53e7bffb24a74ec758b10d56346aa
Darshika <136146993+darshikapanwar009@users.noreply.github.com>
2023-09-05 08:04:55 -0700

    progm5.java

---
+++ progm5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class alpha
+  {
+    public static void main(String[]args)
+    {
+    Scanner sc= new Scanner(System.in);
+       char ch=sc.next().charAt(0);
+      if(ch=='a'|| ch=='i'|| ch=='e'|| ch=='o' || ch=='u')
+      {
+        System.out.println("entered alphabet is vowel");
+      }
+      else{
+        System.out.println("entered alphabet is constant");
+      }
+      
+    }
+  }
