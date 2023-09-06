33dec6dd6a312e6f8278f6b6c55a4ba49faece76
Darshika <136146993+darshikapanwar009@users.noreply.github.com>
2023-09-05 22:56:57 -0700

    progm2.java

--- progm2.java
+++ progm2.java
@@ -1,3 +1,23 @@
-class data
+class demo
 {
-  
+    public static void main(String[]args)
+    {
+        byte b= 114;
+        System.out.println("byte value="+b);
+        int a=-2;
+        System.out.println("integer value="+a);
+        char ch='A';
+        System.out.println("character value="+ch);
+        short s= 9000;
+        System.out.println("Short value="+s);
+        long m= 94011184320L;
+        System.out.println("long value="+m);
+        boolean n=true;
+        System.out.println("Boolean value="+n);
+        float w= 6.66689f;
+        System.out.println("Float value="+w);
+        double D= 6.0d;
+        System.out.println("Double value="+D);
+        
+    }
+}
