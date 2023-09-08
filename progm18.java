51bb265725a51179b38609f3fadc86b68276bd36
Darshika <136146993+darshikapanwar009@users.noreply.github.com>
2023-09-07 22:30:24 -0700

    progm18.java

---
+++ progm18.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class exp
+{
+    public static void main(String args[])
+    {
+    String s1,s2;
+    Scanner sc= new Scanner(System.in);
+    System.out.println("enter first string:");
+    s1=sc.next();
+    System.out.println("enter first string:");
+    s2=sc.next();
+    String s3=s1+ " "+s2;
+    System.out.println("Concatenation of strings="+s3);
+}
+    
+    
+}
