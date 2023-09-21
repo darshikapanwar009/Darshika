2615ff89845dd5430732ff014b237843cae56e95
Darshika <136146993+darshikapanwar009@users.noreply.github.com>
2023-09-21 21:22:23 +0530

    progrm29.java

---
+++ progrm29.java
@@ -0,0 +1,15 @@
+//to convert the string into lowercase and uppercase
+class LC
+{
+    public static void main(String[]args)
+    {
+        String s= "my mother is my world";
+        String s1=s.toUpperCase();
+        System.out.println(s1);
+        String st= "BEAUTIFUL BUTTERFLY IS IN THE SKY";
+        String sg=st.toLowerCase();
+        System.out.println(sg);
+        
+        
+    }
+}
