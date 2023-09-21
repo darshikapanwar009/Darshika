fa4872988ddaa45ad3c71cb1a8ba949bda46e178
Darshika <136146993+darshikapanwar009@users.noreply.github.com>
2023-09-21 21:19:32 +0530

    progrm28.java

---
+++ progrm28.java
@@ -0,0 +1,12 @@
+//to find the length of a string
+class Leng
+{
+    public static void main(String args[])
+    {
+        String greet="Hello World";
+        System.out.println("String="+greet);
+        int l=greet.length();
+        System.out.println(l);
+        
+    }
+}
