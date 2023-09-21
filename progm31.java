a19e754d5601952be11a36c0e13fef2ce2eea0fc
Darshika <136146993+darshikapanwar009@users.noreply.github.com>
2023-09-21 21:27:01 +0530

    progm31.java

---
+++ progm31.java
@@ -0,0 +1,11 @@
+//to use a function isEmpty
+class Empty
+{
+    public static void main(String args[])
+    {
+        String s="hello Java";
+        boolean b=s.isEmpty();
+        System.out.println(b);
+        
+    }
+}
