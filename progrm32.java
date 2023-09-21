18c80cf276925310862c5cca5549797f7d0dfb4a
Darshika <136146993+darshikapanwar009@users.noreply.github.com>
2023-09-21 21:30:43 +0530

    progrm32.java

---
+++ progrm32.java
@@ -0,0 +1,10 @@
+//which letter is present at that given index
+class D2
+{
+    public static void main(String args[])
+    {
+        String s="java";
+        char s1=s.charAt(0);
+        System.out.println(s1);
+    }
+}
